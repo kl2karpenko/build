c16d4b0f640350f34b316837e7911b03c3f4132e
kl2karpenko <kl2karpenko@gmail.com>
2014-12-19 23:24:19 +0200

    Create project

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7 onLoad"  ><![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie10 lt-ie9 lt-ie8 onLoad"><![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie10 lt-ie9 onLoad"><![endif]-->
+<!--[if IE 9]><!-->
+<html class="no-js lt-ie10 onLoad"><!--<![endif]-->
+<!--[if gt IE 9]><!-->
+<html class="no-js onLoad"><!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=8">
+    <title>Build</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <!--[if lte IE 7]>
+    <link href="/assets/css/ie7.css" rel="stylesheet" type="text/css"/>
+    <![endif]-->
+    <!--[if IE 8]>
+    <link href="/assets/css/ie8.css" rel="stylesheet" type="text/css"/>
+    <script src="//ie7-js.googlecode.com/svn/version/2.1(beta4)/IE8.js"></script>
+    <![endif]-->
+    <script type="text/javascript" async src="/assets/js/html5shiv-printshiv.js"></script>
+    <script type="text/javascript" async src="/assets/js/respond.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
